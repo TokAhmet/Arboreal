7a8359a5f9def3d16534bdd23ec6a69cf4b0700c
TokAhmet <ahmet.ordulu@hotmail.se>
2017-12-18 17:26:13 +0100

    huge update

--- script/script.js
+++ script/script.js
@@ -15,20 +15,3 @@ $(document).ready(function () {
 	});
 });
 
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-
-    var x = document.getElementsByClassName("mySlides");
-    for (var i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {
-    	slideIndex = 1;
-    } 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2000); 
-}
-
